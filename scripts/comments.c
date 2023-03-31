3f1ae01eb09c8068bf3b8dbf99cee1620d7fb912
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-31 12:14:42 +0100

    Create comments.c

---
+++ scripts/comments.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// this is a comment
+
+int main() {
+  printf("comments are cool but i dont make them"); // comment
+  return 0;
+}
+
+/* I am not okay
+Please help me
+I don't know what to do
+Pogchamp gamers */
